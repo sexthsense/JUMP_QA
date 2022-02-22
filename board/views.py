ae04fd11a18b5b85ae21144e2cfb516c59b47137
hera <episodex2@naver.com>
2022-02-22 23:11:34 +0900

    11

--- board/views.py
+++ board/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from .models import Question
 from django.utils import timezone
+
+from .forms import QuestionForm, AnswerForm
+from .models import Question
+
 
 def index(request):
     """
@@ -10,23 +12,49 @@ def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
     return render(request, 'board/question_list.html', context)
-    return HttpResponse("Wellcome") #홈페이지에 나타날 문구
+
 
 def detail(request, question_id):
     """
     board 내용 출력
     """
-    question = get_object_or_404(Question, pk=question_id) # Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'board/question_detail.html', context)
 
+
 def answer_create(request, question_id):
     """
-    board 답변 등록
+    board 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'),
-                               create_date=timezone.now())
-    return redirect('board:detail', question_id=question.id)
-# Create your views here.
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'board/question_detail.html', context)
+
+
+def question_create(request):
+    """
+    board 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('board:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'board/question_form.html', context)
 
